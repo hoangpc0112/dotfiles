540b4d897e26947949ed2e4fb8953fc4f30aff3a
Phạm Chính Hoàng <131797054+hoangpc0112@users.noreply.github.com>
2025-07-28 14:16:10 +0700

    update install scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-update_system() {
+setup_mirrot_list() {
+  echo "Setting up mirror list ..."
   sudo pacman -S --noconfirm reflector
-  sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
   sudo reflector --country Vietnam,Singapore,China --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
   sudo pacman -Syyu --noconfirm
 }
@@ -22,14 +22,14 @@ install_paru() {
 }
 
 install_packages() {
-  echo "Installing/updating packages with paru..."
+  echo "Installing/updating packages ..."
   paru -S --noconfirm hyprland hyprlock hyprpaper hypridle waybar rofi fastfetch fzf starship kitty thunar feh mpv \
     unzip unrar neovim python python-pip nodejs npm jdk-openjdk gcc make bat tar bash-completion wget zoxide curl \
-    zen-browser-bin
+    zen-browser-bin inter-font ttf-jetbrains-mono-nerd
 }
 
 setup_dotfiles() {
-  echo "Setting up dotfiles..."
+  echo "Setting up dotfiles ..."
   rm -rf "$HOME/dotfiles"
 
   git clone https://github.com/hoangpc0112/dotfiles.git "$HOME/dotfiles"
@@ -39,28 +39,25 @@ setup_dotfiles() {
     local target_path="$2"
 
     if [ -e "$target_path" ] || [ -L "$target_path" ]; then
-      echo "Removing existing target: $target_path"
       rm -rf "$target_path"
     fi
-    ln -svf "$source_path" "$target_path"
+    ln -sf "$source_path" "$target_path"
   }
 
-  create_symlink "$HOME/dotfiles/.config/fastfetch" "$HOME/.config/fastfetch"
-  create_symlink "$HOME/dotfiles/.config/hypr" "$HOME/.config/hypr"
-  create_symlink "$HOME/dotfiles/.config/kitty" "$HOME/.config/kitty"
-  create_symlink "$HOME/dotfiles/.config/rofi" "$HOME/.config/rofi"
-  create_symlink "$HOME/dotfiles/.config/waybar" "$HOME/.config/waybar"
+  create_symlink "$HOME/dotfiles/.config/fastfetch/" "$HOME/.config/fastfetch/"
+  create_symlink "$HOME/dotfiles/.config/hypr/" "$HOME/.config/hypr/"
+  create_symlink "$HOME/dotfiles/.config/kitty/" "$HOME/.config/kitty/"
+  create_symlink "$HOME/dotfiles/.config/rofi/" "$HOME/.config/rofi/"
+  create_symlink "$HOME/dotfiles/.config/waybar/" "$HOME/.config/waybar/"
+  create_symlink "$HOME/dotfiles/wallpapers/" "$HOME/wallpapers/"
 
   create_symlink "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
   create_symlink "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+  create_symlink "$HOME/dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
 }
 
-main() {
-  update_system
-  install_paru
-  install_packages
-  setup_dotfiles
-  echo "Installation complete! Please reboot or log out/log in for changes to take effect."
-}
-
-main "$@"
+setup_mirrot_list
+install_paru
+install_packages
+setup_dotfiles
+echo "Installation complete! Please reboot or log out/log in for changes to take effect."
