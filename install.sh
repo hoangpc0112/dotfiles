2aab0d9bff884e499b1af95d439378c03fb25b6a
Phạm Chính Hoàng <131797054+hoangpc0112@users.noreply.github.com>
2025-07-28 15:36:35 +0700

    update install scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-setup_mirrot_list() {
-  echo "Setting up mirror list ..."
-  sudo pacman -S --noconfirm reflector
-  sudo reflector --country Vietnam,Singapore,China --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-  sudo pacman -Syyu --noconfirm
-}
-
 install_paru() {
   sudo pacman -S --noconfirm --needed base-devel git
 
@@ -25,38 +18,15 @@ install_packages() {
   echo "Installing/updating packages ..."
   paru -S --noconfirm hyprland hyprlock hyprpaper hypridle waybar rofi fastfetch fzf starship kitty thunar feh mpv \
     unzip unrar neovim python python-pip nodejs npm jdk-openjdk gcc make bat tar bash-completion wget zoxide curl \
-    zen-browser-bin inter-font ttf-jetbrains-mono-nerd
+    zen-browser-bin inter-font ttf-jetbrains-mono-nerd stow
 }
 
 setup_dotfiles() {
   echo "Setting up dotfiles ..."
-  rm -rf "$HOME/dotfiles"
-
-  git clone https://github.com/hoangpc0112/dotfiles.git "$HOME/dotfiles"
-
-  create_symlink() {
-    local source_path="$1"
-    local target_path="$2"
-
-    if [ -e "$target_path" ] || [ -L "$target_path" ]; then
-      rm -rf "$target_path"
-    fi
-    ln -sf "$source_path" "$target_path"
-  }
-
-  create_symlink "$HOME/dotfiles/.config/fastfetch/" "$HOME/.config/fastfetch/"
-  create_symlink "$HOME/dotfiles/.config/hypr/" "$HOME/.config/hypr/"
-  create_symlink "$HOME/dotfiles/.config/kitty/" "$HOME/.config/kitty/"
-  create_symlink "$HOME/dotfiles/.config/rofi/" "$HOME/.config/rofi/"
-  create_symlink "$HOME/dotfiles/.config/waybar/" "$HOME/.config/waybar/"
-  create_symlink "$HOME/dotfiles/wallpapers/" "$HOME/wallpapers/"
-
-  create_symlink "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
-  create_symlink "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
-  create_symlink "$HOME/dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
+  cd ~/dotfiles
+  stow -t $HOME .
 }
 
-setup_mirrot_list
 install_paru
 install_packages
 setup_dotfiles
