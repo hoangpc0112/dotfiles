68224cb08f45ec69ad319aea013d8722d1d9ddd2
Phạm Chính Hoàng <131797054+hoangpc0112@users.noreply.github.com>
2025-07-28 22:47:58 +0700

    update install scripts

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ install_packages() {
   echo "Installing/updating packages ..."
   paru -S --noconfirm --needed hyprland hyprlock hyprpaper hypridle waybar rofi fastfetch fzf starship kitty thunar feh mpv \
     unzip unrar neovim python python-pip nodejs npm jdk-openjdk gcc make bat tar bash-completion wget zoxide curl \
-    zen-browser-bin inter-font ttf-jetbrains-mono-nerd
+    zen-browser-bin inter-font ttf-jetbrains-mono-nerd fcitx5 fcitx5-configtool fcitx5-bamboo fcitx5-gtk fcitx5-qt papirus-icon-theme breeze-gtk sddm
 }
 
 setup_dotfiles() {
@@ -35,19 +35,26 @@ setup_dotfiles() {
     ln -sf "$source_path" "$target_path"
   }
 
-  create_symlink "$HOME/dotfiles/.config/fastfetch/" "$HOME/.config/fastfetch/"
-  create_symlink "$HOME/dotfiles/.config/hypr/" "$HOME/.config/hypr/"
-  create_symlink "$HOME/dotfiles/.config/kitty/" "$HOME/.config/kitty/"
-  create_symlink "$HOME/dotfiles/.config/rofi/" "$HOME/.config/rofi/"
-  create_symlink "$HOME/dotfiles/.config/waybar/" "$HOME/.config/waybar/"
-  create_symlink "$HOME/dotfiles/wallpapers/" "$HOME/wallpapers/"
+  create_symlink "$HOME/dotfiles/.config/fastfetch/" "$HOME/.config/fastfetch"
+  create_symlink "$HOME/dotfiles/.config/hypr/" "$HOME/.config/hypr"
+  create_symlink "$HOME/dotfiles/.config/kitty/" "$HOME/.config/kitty"
+  create_symlink "$HOME/dotfiles/.config/rofi/" "$HOME/.config/rofi"
+  create_symlink "$HOME/dotfiles/.config/waybar/" "$HOME/.config/waybar"
+  create_symlink "$HOME/dotfiles/wallpapers/" "$HOME/wallpapers"
 
   create_symlink "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
   create_symlink "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
   create_symlink "$HOME/dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
 }
 
+change_theme() {
+  gsettings set org.gnome.desktop.interface gtk-theme "Breeze-Dark"
+  gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+  gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+}
+
 install_paru
 install_packages
 setup_dotfiles
+change_theme
 echo "Installation complete! Please reboot or log out/log in for changes to take effect."
