e4f2e1d710c56bb5293620d4998d4d201d8e1007
Phạm Chính Hoàng <131797054+hoangpc0112@users.noreply.github.com>
2025-08-15 18:01:46 +0700

    update dots

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ install_packages() {
   paru -Syu --noconfirm
   paru -S --noconfirm --needed hyprland hyprlock hypridle waybar rofi fastfetch fzf starship kitty thunar feh mpv \
     unzip unrar neovim python python-pip nodejs npm jdk-openjdk gcc make bat tar bash-completion wget zoxide curl \
-    zen-browser-bin inter-font ttf-jetbrains-mono-nerd fcitx5 fcitx5-configtool fcitx5-bamboo fcitx5-gtk fcitx5-qt papirus-icon-theme \
+    firefox inter-font ttf-jetbrains-mono-nerd fcitx5 fcitx5-configtool fcitx5-bamboo fcitx5-gtk fcitx5-qt papirus-icon-theme \
     breeze-gtk sddm swww cliphist grimblast wl-clipboard obs-studio vesktop-bin xdg-desktop-portal-hyprland qt6-wayland btop blueman dunst \
-    bibata-cursor-theme-bin ripgrep trash-cli fd ufw lazygit openssh
+    bibata-cursor-theme-bin ripgrep fd ufw lazygit openssh
 }
 
 create_symlink() {
