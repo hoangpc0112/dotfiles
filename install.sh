d6c3fb2f7470653ff53e87d9f11ef567cc11b6ca
hoangpc0112 <phamchinhhoang@gmail.com>
2025-09-02 14:22:51 +0700

    change file manager from thunar(gui) to yazi(tui)

--- install.sh
+++ install.sh
@@ -24,12 +24,13 @@ install_paru() {
 
 install_packages() {
   paru -Syu --noconfirm
-  paru -S --noconfirm --needed hyprland hyprlock hypridle waybar rofi fastfetch fzf starship kitty thunar feh mpv \
+  paru -S --noconfirm --needed hyprland hyprlock hypridle waybar rofi fastfetch fzf starship kitty feh mpv \
     unzip unrar neovim python python-pip nodejs npm jdk-openjdk gcc make bat tar bash-completion wget zoxide curl \
     zen-browser-bin inter-font ttf-jetbrains-mono-nerd fcitx5 fcitx5-configtool fcitx5-bamboo fcitx5-gtk fcitx5-qt papirus-icon-theme \
     breeze-gtk sddm swww cliphist grimblast wl-clipboard obs-studio vesktop-bin xdg-desktop-portal-hyprland qt6-wayland btop blueman dunst \
     bibata-cursor-theme-bin ripgrep fd ufw lazygit openssh zip onlyoffice-bin postman-bin visual-studio-code-bin localsend-bin gammastep tree \
-    brightnessctl
+    brightnessctl yazi
+  ya pkg add kalidyasin/yazi-flavors:tokyonight-night
 }
 
 create_symlink() {
@@ -59,6 +60,7 @@ setup_dotfiles() {
   create_symlink "$HOME/dotfiles/config/hypr/hyprland-performance.conf" "$HOME/.config/hypr/hyprland.conf"
   create_symlink "$HOME/dotfiles/config/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
   create_symlink "$HOME/dotfiles/fzf-preview.sh" "$HOME/fzf-preview.sh"
+  create_symlink "$HOME/dotfiles/config/yazi/theme.toml" "$HOME/.config/yazi/theme.toml"
 
   chmod +x "$HOME/.config/hypr/scripts/*"
   chmod +x "$HOME/fzf-preview.sh"
